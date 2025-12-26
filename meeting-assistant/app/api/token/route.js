cd9ee323563bb75e4e8fd660e96377a52fedfa94
ashadrana1 <ashadranawork@gmail.com>
2025-12-27 02:01:01 +0530

    Initial commit: smart meeting assistant (frontend + backend)

---
+++ meeting-assistant/app/api/token/route.js
@@ -0,0 +1,47 @@
+import { StreamClient } from "@stream-io/node-sdk";
+
+const apiKey = process.env.STREAM_API_KEY;
+const apiSecret = process.env.STREAM_API_SECRET;
+
+export async function POST(request) {
+  try {
+    const { userId } = await request.json();
+
+    if (!apiKey || !apiSecret) {
+      return Response.json(
+        { error: "Missing API credentials" },
+        { status: 500 }
+      );
+    }
+    //this is for my StreamClient
+    const serverClient = new StreamClient(apiKey, apiSecret);
+
+    // Create/upsert the user first
+     const newUser = {
+        id: userId,
+        role: "admin",
+        name: userId,
+      };
+
+    //upsert means insert or update whichever deem necessary
+      await serverClient.upsertUsers([newUser]);
+
+    // Generate token valid for 24 hours
+    const validity = 24 * 60 * 60;
+
+    const token = serverClient.generateUserToken({
+      user_id: userId,
+      validity_in_seconds: validity,
+    });
+
+    return Response.json({ token });
+}
+
+catch(error) {
+    console.error("Token generation error:", error);
+    return Response.json(
+      { error: "Failed to generate token" },
+      { status: 500 }
+    );}
+
+}
